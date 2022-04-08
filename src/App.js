51ffd25ed9157f80785aa373c07969cfa4b0fa87
Yves Faems <yves.faems@gmail.com>
2022-04-07 18:09:57 -0700

    add compose button

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import Header from './app/components/Header';
-
+import Sidebar from './app/components/Sidebar';
 function App() {
   return (
     <div className="App">
--- src/app/components/Sidebar.js
+++ src/app/components/Sidebar.js
@@ -1,10 +1,19 @@
-import React from 'react';
-import styles from './Sidebar.module.css';
+import { Button } from "@mui/material";
+import React from "react";
+import styles from "./Sidebar.module.css";
+
+import AddIcon from "@mui/icons-material/Add";
 
 function Sidebar() {
-  return (
-    <div>Sidebar</div>
-  )
+	return (
+		<div className={styles.sidebar}>
+			<Button  
+                className={styles.sidebar__compose}
+                startIcon={<AddIcon fontSize="large"  />}>
+				COMPOSE
+			</Button>
+		</div>
+	);
 }
 
-export default Sidebar
+export default Sidebar;
