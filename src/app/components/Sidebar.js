763b468c58f39acb1245a0e0bee9b577b430abc9
Yves Faems <yves.faems@gmail.com>
2022-04-11 16:51:50 -0700

    add footer

--- src/app/components/Sidebar.js
+++ src/app/components/Sidebar.js
@@ -1,4 +1,4 @@
-import { Button } from "@mui/material";
+import { Button, IconButton } from "@mui/material";
 import React from "react";
 import styles from "./Sidebar.module.css";
 
@@ -11,6 +11,9 @@ import LabelImportantIcon from "@mui/icons-material/LabelImportant";
 import NearMeIcon from "@mui/icons-material/NearMe";
 import NoteIcon from "@mui/icons-material/Note";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import PersonIcon from "@mui/icons-material/Person";
+import DuoIcon from "@mui/icons-material/Duo";
+import PhoneIcon from "@mui/icons-material/Phone";
 
 function Sidebar() {
 	return (
@@ -21,13 +24,31 @@ function Sidebar() {
 			>
 				COMPOSE
 			</Button>
-			<SidebarOption Icon={InboxIcon} title="Inbox" number={54} selected={true}/>
+			<SidebarOption
+				Icon={InboxIcon}
+				title="Inbox"
+				number={54}
+				selected={true}
+			/>
 			<SidebarOption Icon={StarIcon} title="Starred" number={54} />
 			<SidebarOption Icon={AccessTimeIcon} title="Snoozed" number={54} />
 			<SidebarOption Icon={LabelImportantIcon} title="Important" number={54} />
 			<SidebarOption Icon={NearMeIcon} title="Sent" number={54} />
 			<SidebarOption Icon={NoteIcon} title="Drafts" number={54} />
 			<SidebarOption Icon={ExpandMoreIcon} title="More" number={54} />
+			<div className={styles.sidebar__footer}>
+				<div className={styles.sidebar__footerIcons}>
+					<IconButton>
+						<PersonIcon />
+					</IconButton>
+					<IconButton>
+						<DuoIcon />
+					</IconButton>
+					<IconButton>
+						<PhoneIcon />
+					</IconButton>
+				</div>
+			</div>
 		</div>
 	);
 }
