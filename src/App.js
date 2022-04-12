d3ba87ee714801b154bd8fcf10ceac86cc395b34
Yves Faems <yves.faems@gmail.com>
2022-04-12 16:11:02 -0700

    Route setup

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
-import React from 'react';
-import './App.css';
-import Header from './app/components/Header';
-import Sidebar from './app/components/Sidebar';
+import React from "react";
+import "./App.css";
+import Header from "./app/components/Header";
+import Sidebar from "./app/components/Sidebar";
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+
 function App() {
-  return (
-    <div className="App">
-        <Header />
-        <Sidebar />
-    </div>
-  );
+	return (
+		<Router>
+			<div className="App">
+				<Header />
+				<div className="app__body">
+					<Sidebar />
+					<Switch>
+						<Route path="/mail">
+                            <Mail />
+                        </Route>
+						<Route path="/">
+                            <EmailList />
+                        </Route>
+					</Switch>
+				</div>
+			</div>
+		</Router>
+	);
 }
 
 export default App;
