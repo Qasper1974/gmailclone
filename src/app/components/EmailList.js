cef0d6e42945850e6f604aa57ce7c79cb4e38f20
Yves Faems <yves.faems@gmail.com>
2022-04-12 16:51:08 -0700

    add Mail and EmailList components

---
+++ src/app/components/EmailList.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './EmailList.module.css';
+
+function EmailList() {
+  return (
+    <div className={styles.emaillist}>EmailList</div>
+  )
+}
+
+export default EmailList
---
+++ src/app/components/Mail.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './Mail.module.css';
+
+function Mail() {
+  return (
+    <div className={styles.mail}>Mail</div>
+  )
+}
+
+export default Mail
