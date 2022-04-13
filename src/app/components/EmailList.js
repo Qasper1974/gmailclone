cafe48230494a519589894b9e19434bb740b252a
Yves Faems <yves.faems@gmail.com>
2022-04-12 18:05:23 -0700

    styling and adding emaillist settings

--- src/app/components/EmailList.js
+++ src/app/components/EmailList.js
@@ -2,14 +2,18 @@ import {
 	ChevronLeft,
 	ChevronRight,
 	KeyboardHide,
+	LocalOffer,
 	MoreVert,
+	People,
 	Redo,
 	Settings,
 } from "@mui/icons-material";
+import InboxIcon from "@mui/icons-material/Inbox";
 import ArrowDropDown from "@mui/icons-material/ArrowDropDown";
 import { Checkbox, IconButton } from "@mui/material";
 import React from "react";
 import styles from "./EmailList.module.css";
+import Section from "./Section";
 
 function EmailList() {
 	return (
@@ -42,9 +46,11 @@ function EmailList() {
 					</IconButton>
 				</div>
 			</div>
-            <div className={styles.emaillist__sections}>
-                
-            </div>
+			<div className={styles.emaillist__sections}>
+            <Section Icon={InboxIcon} title="primary" color="red" selected />
+            <Section Icon={People} title="Social" color="#1A73E8" selected />
+            <Section Icon={LocalOffer} title="Promotions" color="green" selected />
+			</div>
 		</div>
 	);
 }
