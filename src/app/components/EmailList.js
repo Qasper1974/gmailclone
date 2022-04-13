180af1722d6759229b4c8d823fc42de4cebfd486
Yves Faems <yves.faems@gmail.com>
2022-04-12 17:28:01 -0700

    add email settings left

--- src/app/components/EmailList.js
+++ src/app/components/EmailList.js
@@ -1,9 +1,21 @@
+import { MoreVert, Redo } from '@mui/icons-material';
+import ArrowDropDown from '@mui/icons-material/ArrowDropDown';
+import { Checkbox, IconButton } from '@mui/material';
 import React from 'react';
 import styles from './EmailList.module.css';
 
 function EmailList() {
   return (
-    <div className={styles.emaillist}>EmailList</div>
+    <div className={styles.emaillist}>
+        <div className={styles.emaillist__settings}>
+            <div className={styles.emaillist__settingsLeft}>
+                <Checkbox />
+                <IconButton><ArrowDropDown /></IconButton>
+                <IconButton><Redo /></IconButton>
+                <IconButton><MoreVert /></IconButton>
+            </div>
+        </div>
+    </div>
   )
 }
 
