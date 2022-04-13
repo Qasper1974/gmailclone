05d8356c576f0a084f64f9f6010ce77038884492
Yves Faems <yves.faems@gmail.com>
2022-04-12 17:45:30 -0700

    styling email settings

--- src/app/components/EmailList.js
+++ src/app/components/EmailList.js
@@ -1,22 +1,52 @@
-import { MoreVert, Redo } from '@mui/icons-material';
-import ArrowDropDown from '@mui/icons-material/ArrowDropDown';
-import { Checkbox, IconButton } from '@mui/material';
-import React from 'react';
-import styles from './EmailList.module.css';
+import {
+	ChevronLeft,
+	ChevronRight,
+	KeyboardHide,
+	MoreVert,
+	Redo,
+	Settings,
+} from "@mui/icons-material";
+import ArrowDropDown from "@mui/icons-material/ArrowDropDown";
+import { Checkbox, IconButton } from "@mui/material";
+import React from "react";
+import styles from "./EmailList.module.css";
 
 function EmailList() {
-  return (
-    <div className={styles.emaillist}>
-        <div className={styles.emaillist__settings}>
-            <div className={styles.emaillist__settingsLeft}>
-                <Checkbox />
-                <IconButton><ArrowDropDown /></IconButton>
-                <IconButton><Redo /></IconButton>
-                <IconButton><MoreVert /></IconButton>
+	return (
+		<div className={styles.emaillist}>
+			<div className={styles.emaillist__settings}>
+				<div className={styles.emaillist__settingsLeft}>
+					<Checkbox />
+					<IconButton>
+						<ArrowDropDown />
+					</IconButton>
+					<IconButton>
+						<Redo />
+					</IconButton>
+					<IconButton>
+						<MoreVert />
+					</IconButton>
+				</div>
+				<div className={styles.emaillist__settingsRight}>
+					<IconButton>
+						<ChevronLeft />
+					</IconButton>
+					<IconButton>
+						<ChevronRight />
+					</IconButton>
+					<IconButton>
+						<KeyboardHide />
+					</IconButton>
+					<IconButton>
+						<Settings />
+					</IconButton>
+				</div>
+			</div>
+            <div className={styles.emaillist__sections}>
+                
             </div>
-        </div>
-    </div>
-  )
+		</div>
+	);
 }
 
-export default EmailList
+export default EmailList;
