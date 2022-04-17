4921aea4130e058c25f86950eaa7ec722bbd79c2
Yves Faems <yves.faems@gmail.com>
2022-04-17 15:46:34 -0700

    add mailtools right

--- src/app/components/Mail.js
+++ src/app/components/Mail.js
@@ -1,26 +1,62 @@
-import { ArrowBack, CheckCircle, Delete, Email, Error, LabelImportant, MoreVert, MoveToInbox, WatchLater } from "@mui/icons-material";
+import {
+	ArrowBack,
+	CheckCircle,
+	Delete,
+	Email,
+	Error,
+	ExitToApp,
+	LabelImportant,
+	MoreVert,
+	MoveToInbox,
+	Print,
+	UnfoldMore,
+	WatchLater,
+} from "@mui/icons-material";
 import { IconButton } from "@mui/material";
 import React from "react";
+import { useHistory } from "react-router-dom";
 import styles from "./Mail.module.css";
 
 function Mail() {
+	const history = useHistory();
 	return (
 		<div className={styles.mail}>
 			<div className={styles.mail__tools}>
 				<div className={styles.mail__toolsleft}>
-                <IconButton><ArrowBack/></IconButton>
-                <IconButton><MoveToInbox/></IconButton>
-                <IconButton><Error/></IconButton>
-                <IconButton><Delete/></IconButton>
-                <IconButton><Email/></IconButton>
-                <IconButton><WatchLater/></IconButton>
-                <IconButton><CheckCircle/></IconButton>
-                <IconButton><LabelImportant/></IconButton>
-                <IconButton><MoreVert/></IconButton>
+					<IconButton onClick={() => history.push("/")}>
+						<ArrowBack />
+					</IconButton>
+					<IconButton>
+						<MoveToInbox />
+					</IconButton>
+					<IconButton>
+						<Error />
+					</IconButton>
+					<IconButton>
+						<Delete />
+					</IconButton>
+					<IconButton>
+						<Email />
+					</IconButton>
+					<IconButton>
+						<WatchLater />
+					</IconButton>
+					<IconButton>
+						<CheckCircle />
+					</IconButton>
+					<IconButton>
+						<LabelImportant />
+					</IconButton>
+					<IconButton>
+						<MoreVert />
+					</IconButton>
+				</div>
+				<div className={styles.mail__toolsright}>
+                <IconButton><UnfoldMore/></IconButton>
+                <IconButton><Print /></IconButton>
+                <IconButton><ExitToApp/></IconButton>
                 </div>
-				<div className={styles.mail__toolsright}></div>
 			</div>
-            
 		</div>
 	);
 }
