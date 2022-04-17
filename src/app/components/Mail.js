02c756376b6525c00fd1b8d9bcc1d40e4eed7a6b
Yves Faems <yves.faems@gmail.com>
2022-04-16 19:47:15 -0700

    add mail tools

--- src/app/components/Mail.js
+++ src/app/components/Mail.js
@@ -1,10 +1,28 @@
-import React from 'react';
-import styles from './Mail.module.css';
+import { ArrowBack, CheckCircle, Delete, Email, Error, LabelImportant, MoreVert, MoveToInbox, WatchLater } from "@mui/icons-material";
+import { IconButton } from "@mui/material";
+import React from "react";
+import styles from "./Mail.module.css";
 
 function Mail() {
-  return (
-    <div className={styles.mail}>Mail</div>
-  )
+	return (
+		<div className={styles.mail}>
+			<div className={styles.mail__tools}>
+				<div className={styles.mail__toolsleft}>
+                <IconButton><ArrowBack/></IconButton>
+                <IconButton><MoveToInbox/></IconButton>
+                <IconButton><Error/></IconButton>
+                <IconButton><Delete/></IconButton>
+                <IconButton><Email/></IconButton>
+                <IconButton><WatchLater/></IconButton>
+                <IconButton><CheckCircle/></IconButton>
+                <IconButton><LabelImportant/></IconButton>
+                <IconButton><MoreVert/></IconButton>
+                </div>
+				<div className={styles.mail__toolsright}></div>
+			</div>
+            
+		</div>
+	);
 }
 
-export default Mail
+export default Mail;
