c8cf0d5453c939ceb528081e70478dd0a7c405ae
Yves Faems <yves.faems@gmail.com>
2022-04-21 12:13:37 -0700

    persisting login state

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 import "./App.css";
 
@@ -7,14 +7,31 @@ import Sidebar from "./app/components/Sidebar";
 import Mail from "./app/components/Mail";
 import EmailList from "./app/components/EmailList";
 import SendMail from "./app/components/SendMail";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { selectSendMessageIsOpen } from "./features/mailSlice";
-import { selectUser } from "./features/userSlice";
+import { login, selectUser } from "./features/userSlice";
 import Login from "./app/components/Login";
+import { auth } from "./app/firebase";
 
 function App() {
 	const sendMessageIsOpen = useSelector(selectSendMessageIsOpen);
 	const user = useSelector(selectUser);
+    const dispatch = useDispatch();
+
+    //to persist login state
+    useEffect(() => {
+      auth.onAuthStateChanged(user => {
+          if (user) {
+              dispatch(login({
+                displayName: user.displayName,
+                email: user.email,
+                photoUrl: user.photoURL,
+            }))
+          }
+      });
+    
+    }, []);
+
 
 	return (
 		<Router>
--- src/app/components/Login.js
+++ src/app/components/Login.js
@@ -20,7 +20,7 @@ function Login() {
 		})
         .catch((error)=> {
             alert(error.message)
-        });
+        }); 
 	};
 	return (
 		<div className={styles.login}>
